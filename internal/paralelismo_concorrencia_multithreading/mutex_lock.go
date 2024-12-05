d232a2f4dee5cb5416f55818a45576d90e6ace4f
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-12-05 18:22:18 -0300

    feat: implement MutexLock function for order processing with Redis

---
+++ internal/paralelismo_concorrencia_multithreading/mutex_lock.go
@@ -0,0 +1,107 @@
+package paralelismo_concorrencia_multithreading
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	redis "github.com/redis/go-redis/v9"
+)
+
+// ctx is a global variable that holds a background context.
+// It is used as a base context for creating other contexts in the application.
+// Background contexts are typically used for top-level contexts in main functions, initialization, and tests.
+var ctx = context.Background()
+
+// PedidoDeCompra represents a purchase order with an ID, item name, and quantity.
+type PedidoDeCompra struct {
+	Id         string
+	Item       string
+	Quantidade int
+}
+
+// MutexLock is a function that handles the processing of a new order message
+// with a mutex lock mechanism using Redis. It ensures that the resource
+// identified by the order ID is locked before processing to prevent concurrent
+// access. The lock is set with a timeout of 10 seconds. If the resource is
+// already locked, the function will print a message and return. After
+// processing the order, the lock is released.
+func MutexLock() {
+	conn := connRedis("localhost:6379", "", 0)
+
+	NovoPedido := consomeMensagem()
+	mutexKey := NovoPedido.Id
+
+	lock, _ := conn.Get(ctx, mutexKey).Result()
+	if lock != "" {
+		fmt.Println("Mutex travado para o recurso", mutexKey)
+		return
+	}
+
+	err := conn.Set(ctx, mutexKey, "loocked", 10*time.Second).Err()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Mutex criado para o recurso por 10s: ", mutexKey)
+
+	success := processaMenssagem(NovoPedido)
+	if !success {
+		fmt.Println("Erro ao processar pedido", NovoPedido.Id)
+	}
+	fmt.Println("Pedido processado: ", NovoPedido.Id)
+
+	_, err = conn.Del(ctx, mutexKey).Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Mutex liberado para o recurso: ", mutexKey)
+}
+
+// processaMenssagem processes a purchase order by printing its ID and simulating
+// a delay of 1 second to represent the processing time. It returns true to indicate
+// that the processing was successful.
+//
+// Parameters:
+//
+//	pedido (PedidoDeCompra): The purchase order to be processed.
+//
+// Returns:
+//
+//	bool: Always returns true to indicate successful processing.
+func processaMenssagem(pedido PedidoDeCompra) bool {
+	fmt.Println("Processando pedido", pedido.Id)
+	time.Sleep(1 * time.Second)
+
+	return true
+}
+
+// consomeMensagem simulates the consumption of a purchase order message.
+// It returns a PedidoDeCompra struct with predefined values.
+func consomeMensagem() PedidoDeCompra {
+	return PedidoDeCompra{
+		Id:         "123",
+		Item:       "Camisa",
+		Quantidade: 2,
+	}
+}
+
+// connRedis creates and returns a new Redis client with the specified
+// address, password, and database index. It uses the provided parameters
+// to configure the Redis client options.
+//
+// Parameters:
+//
+//	addr - the address of the Redis server
+//	pass - the password for the Redis server
+//	db   - the database index to be selected after connecting
+//
+// Returns:
+//
+//	*redis.Client - a pointer to the newly created Redis client
+func connRedis(addr string, pass string, db int) *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: pass,
+		DB:       db,
+	})
+}
